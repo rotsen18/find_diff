22159f58d4753cc84047f99736d8fecde84b0d4b
Taras Nester <tarasnester414@gmail.com>
2022-05-13 21:07:56 +0300

    added functions

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
-from os import listdir, linesep
+from os import listdir
 from os.path import isfile, join
 
-path = '../files_to_check'
-file_names = [f for f in listdir(path) if isfile(join(path, f))]
+
+PATH = '../files_to_check'
 
 
-files = {}
-for name in file_names:
-    file = open(join(path, name), 'r')
-    file_dict = {}
-    for line in file:
-        if not line.startswith('#'):
-            item_name, item_value = line.rsplit(' ', 1)
-            file_dict[item_name] = item_value
-    files[name] = file_dict
-    file.close()
+def get_data(path):
+    file_names = [f for f in listdir(path) if isfile(join(path, f))]
+    files = {}
+    for name in file_names:
+        file = open(join(path, name), 'r')
+        file_dict = {}
+        for line in file:
+            if not line.startswith('#'):
+                item_name, item_value = line.rsplit(' ', 1)
+                file_dict[item_name] = item_value
+        files[name] = file_dict
+        file.close()
+
+    return files
 
 
-def get_base_file_name(message):
+def get_base_file_name(message, files):
     print("List of files:")
     position = 1
     names_with_numbers = {}
@@ -28,41 +32,43 @@ def get_base_file_name(message):
     print()
     choice = input(message)
     if not choice.isdigit():
-        get_base_file_name('Probably typo in your input. Write only number end press enter: ')
+        get_base_file_name('Probably typo in your input. Write only number end press enter: ', files)
     if 1 < int(choice) < 2:
-        get_base_file_name('Choose from 1 or 2: ')
+        get_base_file_name('Choose from 1 or 2: ', files)
 
     return names_with_numbers[int(choice)]
 
 
-base_file_name = get_base_file_name("Which file should be base? ")
-print("Base file is:", base_file_name)
-base_file = set(files.pop(base_file_name))
-file_with_changes = set(list(files.values())[0])
-print("names in base file:", len(base_file))
-print("names in other file:", len(file_with_changes))
-print()
+def print_result(name_list, list_type):
+    print(f'{len(name_list)} {list_type} names:')
+    result_file_name = list_type + '.txt'
+    with open(result_file_name, 'w') as f1:
+        f1.writelines(f'{len(name_list)} {list_type} names:\n')
+        for name in name_list:
+            name = name.replace('\n', '')
+            print(name)
+            f1.writelines(name + '\n')
+    print('_' * 50, '\n')
 
 
-def print_result(name_list):
-    for name in name_list:
-        print(name)
+def main():
+    files = get_data(PATH)
+    base_file_name = get_base_file_name("Which file should be base? ", files)
+
+    print("Base file is:", base_file_name)
+    base_file = set(files.pop(base_file_name))
+    file_with_changes = set(list(files.values())[0])
+    print("names in base file:", len(base_file))
+    print("names in other file:", len(file_with_changes))
+
+    print()
+    print("Now I check to file for difference...")
+    deleted_names = base_file.difference(file_with_changes)
+    added_names = file_with_changes.difference(base_file)
+    print(f'You have {len(added_names)} new names and {len(deleted_names)} deleted\n')
+
+    print_result(added_names, 'added')
+    print_result(deleted_names, 'deleted')
 
 
-
-print("Now I check to file for difference...")
-deleted_names = base_file.difference(file_with_changes)
-added_names = file_with_changes.difference(base_file)
-print(f'You have {len(added_names)} new names and {len(deleted_names)} deleted\n')
-print(f'+1 {len(added_names)} Added names:')
-print_result(added_names)
-print('_' * 50, '\n')
-print(f'-{len(deleted_names)} Deleted names:')
-print_result(deleted_names)
-print('_' * 50, '\n')
-with open('added.txt', 'w') as f1:
-    for name in added_names:
-        f1.writelines(name + '\n')
-# with open('deleted.txt', 'w') as f1:
-#     for name in deleted_names:
-#         f1.writelines(name)
+main()
